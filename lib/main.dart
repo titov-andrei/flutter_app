e920a43cdd866bd2fb91702f64dd8e2f628127fd
titov-andrei <titov_andrei@mail.ru>
2020-09-21 17:45:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(MaxFitApp());
+
+class MaxFitApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Max Fitness',
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(50, 65, 85, 1),
+        textTheme: TextTheme(title: TextStyle(color: Colors.white))
+      ),
+      home: Text('Hi')
+    );
+  }
+}
