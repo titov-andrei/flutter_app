5ef885808c4cc7356ab1034fc039ee9e11c20f55
titov-andrei <titov_andrei@mail.ru>
2020-10-12 15:39:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,12 @@ class MaxFitApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Max Fitness',
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(50, 65, 85, 1),
-        textTheme: TextTheme(title: TextStyle(color: Colors.white))
-      ),
-      home: HomePage()
+        title: 'Max Fitness',
+        theme: ThemeData(
+            primaryColor: Color.fromRGBO(50, 65, 85, 1),
+            textTheme: TextTheme(title: TextStyle(color: Colors.white))
+        ),
+        home: HomePage()
     );
   }
 }
